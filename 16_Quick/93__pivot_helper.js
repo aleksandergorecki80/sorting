a84d554b20485825b9f030552d99ce711f7f9df8
Aleksander <aleksandergorecki80@gmail.com>
2022-09-09 20:29:33 +0200

    Quick sort not working

--- 16_Quick/93__pivot_helper.js
+++ 16_Quick/93__pivot_helper.js
@@ -1,10 +1,10 @@
-const arr1 = [8, 4, 9, 2, 1, 5, 7, 6, 3];
+const arr1 = [4, 9, 2, 1, 5, 7, 6, 3];
 
 const swap = (arr, idx1, idx2) => {
   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 };
 
-const pivotHelper = (arr, start = 0, end = arr.length + 1) => {
+const getPivotIndex = (arr, start = 0, end = arr.length + 1) => {
   let pivot = arr[start];
   let swapIndex = start;
 
@@ -17,4 +17,17 @@ const pivotHelper = (arr, start = 0, end = arr.length + 1) => {
   return swapIndex;
 };
 
-console.log(pivotHelper(arr1))
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = getPivotIndex(arr, left, right);
+
+    // LEFT
+    quickSort(arr, left, pivotIndex - 1);
+
+    // RIGHT
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort(arr1));
